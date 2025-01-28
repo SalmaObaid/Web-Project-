3b80bc0bc359d4d7c63006780a2d77bec42128ce
Salma Obaid <salmaobaid@Salmas-MacBook-Air.local>
2025-01-28 11:55:34 +0300

    remove isLoggedin fun from dashboard router

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,7 +2,8 @@
  const router = express.Router();
  const {isLoggedIn} = require('../middleware/checkAuth');
  const dashboardController = require('../controllers/dashboardController');
- 
- router.get('/dashboard',isLoggedIn, dashboardController.dashboard); //to redirect user must be logged in
+
+ // add isLoggedIn 
+ router.get('/dashboard', dashboardController.dashboard); //to redirect user must be logged in
 
   module.exports=router; 
